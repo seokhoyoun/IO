a99a4039a4ad0f0017e8c6f0543e5685af0971df
Seokho Youn <axlover@hotmail.com>
2018-12-11 20:08:28 +0900

    init

---
+++ src/week4/io/silsub/TestSilsub1.java
@@ -0,0 +1,17 @@
+package week4.io.silsub;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class TestSilsub1 {
+	public static void main(String[] args) {
+		fileSave();
+	}
+	
+	public static void fileSave() {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+	
+	
+}
