b4b129c8f05dba0b32993b1343872f293185fda7
Seokho Youn <axlover@hotmail.com>
2018-12-14 12:34:16 +0900

    오류 수정
    바이트에서 문자스트림이용

--- src/week4/io/silsub/TestSilsub1.java
+++ src/week4/io/silsub/TestSilsub1.java
@@ -2,57 +2,54 @@ package week4.io.silsub;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class TestSilsub1 {
+	private Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-//		fileSave();
 		TestSilsub1 test = new TestSilsub1();
+//		test.fileSave();
 		test.fileRead();
 	}
 	
-	public static void fileSave() {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		FileOutputStream fin = null;
-		try {
-		System.out.print("파일명 입력 : ");
-		String fileName = in.readLine();
-			fin = new FileOutputStream(fileName);
-			System.out.println("프로젝트 작성을 시작합니다.\n끝내시려면 exit를 입력하세요.");
-			String line = null;
-			while(!(line = in.readLine()).equals("exit")) {
-				fin.write(line.getBytes());
+	public void fileSave() {
+		System.out.print("저장할 파일명 : ");
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(sc.next()))){
+			System.out.println("저장할 내용을 입력하세요.(종료 = exit)");
+			sc.nextLine(); // 버퍼 엔터키 제거
+			String line;
+			while(!(line = sc.nextLine()).equals("exit")) {
+				bw.write(line);
+				bw.newLine(); // 개행 처리
 			}
-			
-		} catch (FileNotFoundException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				fin.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 	}
 	
 	public void fileRead() {
 		System.out.print("불러올 파일명 입력 : ");
-		StringBuilder sb = new StringBuilder();
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		try(BufferedInputStream bis = new BufferedInputStream(new FileInputStream(in.readLine()))){
-			int data;
-			while((data = bis.read()) != -1) {
-				sb.append((char)data);
-			}
-			System.out.println(sb.toString());
+		try (BufferedReader br = new BufferedReader(new FileReader(sc.next()))){
+			StringBuilder sb = new StringBuilder();
+				String line = null;
+				while((line = br.readLine()) != null) {
+					sb.append(line);
+				}
+				System.out.println(sb.toString());
+			
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	
 	}
 }
